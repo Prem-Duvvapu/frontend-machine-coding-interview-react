b8ceab37c338f7ca5e7d810de8f589acfe11be9d
Prem-Duvvapu <prem.duvvapu@gmail.com>
2025-08-06 12:22:56 +0000

    added animation

--- progress-bar/src/App.jsx
+++ progress-bar/src/App.jsx
@@ -1,16 +1,26 @@
+import { useEffect, useState } from 'react';
 import './style.css';
 
 const ProgressBar = ({ progress }) => {
+  const [ animatedProgress, setAnimatedProgress ] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAnimatedProgress(progress);
+    },100);
+  }, [progress]);
+
   return (
     <div className="outer">
         <div 
           style={{ 
-            width: `${progress}%`, 
-            color: progress<5 ? "black" : "white" 
+            // width: `${animatedProgress}%`, //this will repaint css on page again and again
+            transform: `translateX(${animatedProgress - 100}%)`, //this will translate. (optimized)
+            color: animatedProgress<5 ? "black" : "white" 
           }} 
           className="inner"
           role="progressbar"
-          aria-valuenow={progress}
+          aria-valuenow={animatedProgress}
           aria-valuemin={0}
           aria-valuemax={100}
         >
